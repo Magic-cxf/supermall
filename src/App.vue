0c52283ae968280e757e7d6aba27e2d733f3f3d6
MagicSqe <MagicSqe0606@petalmail.com>
2022-05-08 22:11:11 +0800

    modify app

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
-
+  <div>
+    <h1>welcome to my supermall</h1>
+    <p>test git </p>
+  </div>
 </template>
+
 <script>
-
 export default {
-  name: 'App',
-  components: {
-
-  }
+  name:"App",
+  data () {
+    return {
+    };
+  },
 }
+
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style >
 </style>
