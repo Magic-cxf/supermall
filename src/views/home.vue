2788dc37eaf09f3693817b7f852f08233ab4feb2
MagicSqe <MagicSqe0606@petalmail.com>
2022-05-14 23:10:29 +0800

    increase element plus

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,18 @@
 <template>
-    <div>
-      <h1>supermall</h1>
+    <div class="home">
+      <head-line></head-line>
+      <carousel></carousel>
     </div>
 </template>
 
 <script>
+import headLine from './homeChildren/headLine.vue'
+import carousel from 'components/common/carousel/carousel.vue'
 export default {
+  components:{
+    headLine,
+    carousel
+  },
   data () {
     return {
     };
---
+++ src/views/homeChildren/headLine.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+      <div class="left"></div>
+      <div class="center">
+          supermall
+      </div>
+      <div class="right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+}
+
+</script>
+<style scoped>
+.header{
+    width: 100%;
+    height: 44px;
+    display: flex;
+    background-color: pink;
+}
+.left{
+    width: 60px;
+}
+.center{
+    flex: 1;
+}.center{
+    text-align: center;
+    line-height: 44px;
+    font-size: 25px;
+}
+.right{
+    width: 60px;
+}
+</style>
